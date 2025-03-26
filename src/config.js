48064e71321038281a4007aa73be3c1f6ef9b792
Collins-Mibei <cmibei@kabarak.ac.ke>
2025-03-26 12:12:05 +0300

    Update github link

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/Mibei-Collins',
+      url: 'https://github.com/Collins-Mibei',
     },
     {
       name: 'Linkedin',
